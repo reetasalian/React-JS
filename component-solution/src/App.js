dddd6471d8459b33cc0014f1b58b7d411bfb883c
Reeta Salian <reeta.sonar@gmail.com>
2023-02-27 12:30:55 +0000

    component solution added

---
+++ component-solution/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+import TopNav from "./TopNav";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <TopNav />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ component-solution/src/Main.js
@@ -0,0 +1,20 @@
+const Main = () => {
+    return (
+      <div
+        style={{
+          height: 'calc(100vh - 220px)',
+          background: "#fab7fa",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "50px",
+          fontWeight: "bold",
+          color: "purple"
+        }}
+      >
+        React Components
+      </div>
+    );
+  };
+  
+  export default Main;
